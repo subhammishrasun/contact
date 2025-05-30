158e4badf523feec532500fb4e0259134f585ff4
Shubham mishra <121163825+subhammishrasun@users.noreply.github.com>
2025-05-30 11:52:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 
 // Initialize app
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 // Middleware
 app.use(cors());
